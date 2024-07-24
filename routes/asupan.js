dc2870476ac88551618e8ec6ea89f108fb9b5c71
SatzzDev <94371633+SatzzDev@users.noreply.github.com>
2024-07-24 23:45:44 +0700

    Update asupan.js

--- routes/asupan.js
+++ routes/asupan.js
@@ -1,5 +1,49 @@
+import cheerio from 'cheerio';
+import got from 'got';
 import axios from "axios"
-import cheerio from "cheerio"
+
+async function downloadUrlebird(URL) {
+try {
+const response = await got(URL);
+const $ = cheerio.load(response.body);
+const videoURL = $('meta[property="og:video"]').attr('content');
+return videoURL;
+} catch (error) {
+throw new Error(error.message);
+}
+}
+
+export async function randomVid(URL) {
+try {
+const response = await got(URL);
+const $ = cheerio.load(response.body);
+const results = [];
+const htmlSegment = response.body.split('<div class="col-md-12 text-center text-md-left p-2 pl-md-5 pl-0" id="thumbs">')[1]
+.split('<div class="row">\n<div class="col-md-12 mt-1 justify-content-center text-center">')[0];
+const $segment = cheerio.load(htmlSegment);
+$segment('div.thumb.wc .info3').each((index, element) => {
+const links = $(element).find('a');
+if (links.length > 1) {
+const href = links.eq(1).attr('href');
+if (href && href.includes('.com/id/video/')) {
+results.push(href);
+}
+}
+});
+let result = []
+for (const u of results) {
+result.push(await downloadUrlebird(u))
+}
+return result;
+} catch (error) {
+throw new Error(error.message);
+}
+}
+
+
+// randomVid('https://urlebird.com/id/user/ph0nk0ne/')
+// .then(console.log)
+// .catch(console.error);
 
 export const asupan = async() => {
 try {
@@ -16,10 +60,10 @@ const usernames = [
 ];
 const username =
 usernames[Math.floor(Math.random() * usernames.length)];
-const stalkResponse = await axios.get(
+const stalkResponse = await got(
 `https://urlebird.com/user/${username}/`,
 );
-const stalkHtml = stalkResponse.data;
+const stalkHtml = stalkResponse.body;
 const $stalk = cheerio.load(stalkHtml);
 const linkVideo = [];
 const udahAda = {};
@@ -35,10 +79,7 @@ const randomVideoLink = linkVideo[randomIndex] || linkVideo[0];
 if (!randomVideoLink) {
 throw new Error("No video link found");
 }
-const getVidResponse = await axios.get(randomVideoLink);
-const getVidHtml = getVidResponse.data;
-const $getVid = cheerio.load(getVidHtml);
-const videoSrc = $getVid(".video_html5 video").attr("src");
+const videoSrc = await downloadUrlebird(randomVideoLink);
 if (!videoSrc) {
 throw new Error("Video src not found");
 }
@@ -51,4 +92,4 @@ console.error("Error in asupan request:", error.message);
 throw new Error(`Request failed: ${error.message}`);
 }
 }
-//asupan().then(console.log).catch(console.error);
+asupan().then(console.log).catch(console.error);
