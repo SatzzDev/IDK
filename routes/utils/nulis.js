45edbe452fa6447ca17767dd5175a738945d950e
SatzzDev <94371633+SatzzDev@users.noreply.github.com>
2025-01-18 13:21:22 +0000

    hehee

--- routes/utils/nulis.js
+++ routes/utils/nulis.js
@@ -11,7 +11,7 @@ args: ["--no-sandbox", "--disable-setuid-sandbox"]
 const page = await browser.newPage();
 await page.setViewport({width: 1080, height: 1080, deviceScaleFactor: 2});
 try {
-await page.goto(`https://ahmadghozali-xyz.github.io`, {waitUntil: "networkidle2"});
+await page.goto(`https://ahmadghozali-xyz.github.io`, {waitUntil: "networkidle2",timeout: 60000,});
 await page.type("#date", tanggal);
 await page.type("#name", nama);
 await page.type("#content", text);
